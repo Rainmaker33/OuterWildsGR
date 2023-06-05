de9f29ac3e4177321623d8e6c552530b69d6940f
Rainmaker33 <bilos1993@gmail.com>
2023-06-05 12:34:53 +0300

    Initial commit

---
+++ ILocalizationAPI.cs
@@ -0,0 +1,12 @@
+using OWML.ModHelper;
+using System;
+
+namespace OuterWildsGR
+{
+    public interface ILocalizationAPI
+    {
+        void RegisterLanguage(ModBehaviour mod, string name, string translationPath);
+        void AddLanguageFont(ModBehaviour mod, string name, string assetBundlePath, string fontPath);
+        void AddLanguageFixer(string name, Func<string, string> fixer);
+    }
+}
---
+++ OuterWildsGR.cs
@@ -0,0 +1,19 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace OuterWildsGR
+{
+	public class OuterWildsGR : ModBehaviour
+	{
+		public static OuterWildsGR Instance;
+
+		private void Start()
+		{
+			// Starting here, you'll have access to OWML's mod helper.
+			ModHelper.Console.WriteLine($"My mod {nameof(OuterWildsGR)} is loaded!", MessageType.Success);
+			var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+			api.RegisterLanguage(this,"Greek", "assets/Translation.xml");
+			api.AddLanguageFont(this,"Greek","assets/murecho-medium","Assets/Murecho-Medium.otf");
+		}
+	}
+}
